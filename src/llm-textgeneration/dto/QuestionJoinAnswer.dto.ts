2e03673d60eb5e2c415c75721cac9998f141c046
ItzelCabrera <icabrerav1600@alumnno.ipn.mx>
2024-12-02 00:02:15 -0600

    modifica dtos

--- src/llm-textgeneration/dto/QuestionJoinAnswer.dto.ts
+++ src/llm-textgeneration/dto/QuestionJoinAnswer.dto.ts
@@ -9,4 +9,7 @@ export class QuestionJoinAnswerDTO{
 
     @IsString()
     readonly answerUser: string;
+
+    @IsString()
+    readonly answerLLM: string;
 }
--- src/llm-textgeneration/dto/update-llm-textgeneration.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLlmTextgenerationDto } from './create-llm-textgeneration.dto';
-
-export class UpdateLlmTextgenerationDto extends PartialType(CreateLlmTextgenerationDto) {}
